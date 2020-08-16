41cfe855fc29a3859a498382b381b98e36a9055b
mholta <68783911+mholta@users.noreply.github.com>
2020-08-16 13:46:05 +0200

    Update video-script.js

--- video-script.js
+++ video-script.js
@@ -80,3 +80,48 @@ function initVidThumbs(iframe) {
       }
     })
 };
+
+// LAZY LOADING PART
+
+  var spotifyID = ""
+  var spotifyTarget = document.getElementById("spotify-embed-target")
+  var preVid = document.getElementById("pre-vid")
+  var spotifyLoaded = false
+  var vidLoaded = false
+
+function lazySpotify() {
+	if (! spotifyLoaded && isInViewport(spotifyTarget)) {
+  	spotifyTarget.innerHTML = '<iframe src="https://open.spotify.com/embed/track/' + spotifyID + '" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+		spotifyLoaded = true
+    window.removeEventListener('scroll', lazySpotify, false)
+    window.removeEventListener('resize', lazySpotify, false)
+  }
+}
+
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= -100 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
+function lazyVideos() {
+	if (! spotifyLoaded && isInViewport(spotifyTarget)) {
+		VideoLoaded = true
+    initVidThumbs(true)
+    window.removeEventListener('scroll', lazyVideos, false)
+    window.removeEventListener('resize', lazyVideos, false)
+  }
+}
+
+function initLazy() {
+  lazySpotify()
+  window.addEventListener('scroll', lazySpotify, false)
+  window.addEventListener('resize', lazySpotify, false)
+
+  window.addEventListener('scroll', lazyVideos, false)
+  window.addEventListener('resize', lazyVideos, false)
+}
